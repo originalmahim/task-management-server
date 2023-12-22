e2c1ca8f9e1611219f739396b0fb19d0c76fdcf3
originalmahim <tareqazizmahim@gmail.com>
2023-12-22 19:42:38 +0600

    Created api for posting task successfully

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eqvmyxo.mongodb.net/?retryWrites=true&w=majority`;
@@ -15,19 +19,18 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const taskFile = client.db('taskFile').collection('taskCollection');
+
+app.post('/task', async (req, res) => {
+          try {
+            const product = req.body;
+            const result = await taskFile.insertOne(product);
+            res.send(result);
+          } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
+          }
+        });
 
 
 app.get('/', (req, res) => {
